c55debb5931ec198d360490c3b4559948beeaeb8
sanaBth <thayria20@yahoo.com>
2021-09-27 14:30:17 +0100

    2709

---
+++ src/app/challenge6/challenge6.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-challenge6',
+  templateUrl: './challenge6.component.html',
+  styleUrls: ['./challenge6.component.css']
+})
+export class Challenge6Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
